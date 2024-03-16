e32261d2fa54299477e0f818b3636e8a3a515af9
Bmotloch <259301@student.pwr.edu.pl>
2024-03-16 11:37:44 +0100

    Testing basics

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import cv2
+import numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def compress_image(image_path, quality_=20, block_size_=8):
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
+    if img is None:
+        print("Error in reading the image.")
+        exit()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    height, width = img.shape
+    compressed_img = np.zeros_like(img)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for i in range(0, height, block_size_):
+        for j in range(0, width, block_size_):
+            block = img[i:i + block_size_, j:j + block_size_]
+            dct_block = cv2.dct(np.float32(block) / 255.0)
+            dct_block_quantized = np.round(dct_block / quality_) * quality_
+            compressed_block = cv2.idct(dct_block_quantized) * 255.0
+            compressed_block = np.clip(compressed_block, 0, 255)
+            compressed_img[i:i + block_size_, j:j + block_size_] = np.uint8(compressed_block)
+
+    return compressed_img
+
+if __name__ == "__main__":
+    input_image_path = 'tiles.jpg'
+    output_image_path = 'compressed_tiles.jpg'
+    quality = 1
+    block_size = 8
+    compressed_image = compress_image(input_image_path, quality, block_size)
+
+    cv2.imwrite(output_image_path, compressed_image)
+
+    print("Image compressed and saved successfully.")
+
+    cv2.imshow('Original Image', cv2.resize(cv2.imread(input_image_path), (800, 600)))
+    cv2.imshow('Compressed Image', cv2.resize(compressed_image, (800, 600)))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
